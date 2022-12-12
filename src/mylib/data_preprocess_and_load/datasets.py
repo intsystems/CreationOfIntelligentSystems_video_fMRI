1b1de626b54ab304b68dd1e710207c0014304c0f
pollinab <44142702+pollinab@users.noreply.github.com>
2022-12-13 00:11:49 +0300

    Update datasets.py

--- src/mylib/data_preprocess_and_load/datasets.py
+++ src/mylib/data_preprocess_and_load/datasets.py
@@ -4,6 +4,8 @@ from torch.utils.data import Dataset
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+import cv2
+from torchvision.transforms import ToTensor
 
 
 class fMRIDataset(Dataset):
