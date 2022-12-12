41cdbd4b89823fc32585fd42529399245c9cde0a
pollinab <44142702+pollinab@users.noreply.github.com>
2022-12-13 02:30:17 +0300

    Update trainer.py

--- src/mylib/trainer.py
+++ src/mylib/trainer.py
@@ -1,45 +1,59 @@
 from learning_rate import LrHandler
-#from data_preprocess_and_load.dataloaders import DataHandler
 import torch
 import warnings
 from tqdm import tqdm
-from model import Encoder_Transformer_Decoder,Encoder_Transformer_finetune,AutoEncoder
-from losses import get_intense_voxels
+from model import Encoder_Transformer_Decoder,Encoder_Transformer_Decoder_VAE,AutoEncoder, VAE_with_VTN
+from losses import get_intense_voxels, Percept_Loss
+from data_preprocess_and_load.datasets import fMRIVideoDataset, fMRIDataset
+from torch.utils.data import DataLoader
 
 class Trainer():
     """
     main class to handle training, validation and testing.
     note: the order of commands in the constructor is necessary
     """
-    def __init__(self, train_dataloader, **kwargs):
+    def __init__(self,**kwargs):
         self.register_args(**kwargs)
         self.lr_handler = LrHandler(**kwargs)
-        self.train_loader = train_dataloader
+        if 'video' in self.task.lower():
+          dataset = fMRIVideoDataset(train=True, seq_len=self.seq_len, 
+                                     video_path=self.video_path, skip_frames=self.skip_frames)
+        else:
+          dataset = fMRIDataset(seq_len=self.seq_len)
+        self.train_loader = DataLoader(dataset=dataset, batch_size=self.batch_size, shuffle=True, drop_last=True)
         self.create_model()
         self.initialize_weights(load_cls_embedding=False)
         self.create_optimizer()
         self.lr_handler.set_schedule(self.optimizer)
 
-        #self.initialize_weights(load_cls_embedding=False)
-        #self.writer = Writer(sets,**kwargs)
-        #self.sets = sets
-        #self.eval_iter = 0
+        if 'vae' in self.task.lower():
+            self.use_KL = True
+        else:
+            self.use_KL = False
+            self.KL_factor = 0
+
         self.losses = {'intensity':
                            {'is_active':True,'factor':self.intensity_factor},
                        'perceptual':
                            {'is_active':True, 'factor':self.perceptual_factor},
                        'reconstruction':
-                           {'is_active':True,'factor':self.reconstruction_factor}}
+                           {'is_active':True,'factor':self.reconstruction_factor},
+                       'KL':
+                           {'is_active':self.use_KL,'factor':self.KL_factor}}
         self.reconstruction_loss_func = nn.L1Loss()
-        self.perceptual_loss_func = Percept_Loss(self.memory_constraint)
+        self.perceptual_loss_func = Percept_Loss(self.memory_constraint, self.cuda)
         self.intensity_loss_func = nn.L1Loss() #(thresholds=[0.9, 0.99]
 
     def initialize_weights(self,load_cls_embedding):
         if self.loaded_model_weights_path is not None:
             state_dict = torch.load(self.loaded_model_weights_path)
-            #self.lr_handler.set_lr(state_dict['lr'])
-            self.model.load_partial_state_dict(state_dict['model_state_dict'],load_cls_embedding)
-            #self.model.loaded_model_weights_path = self.loaded_model_weights_path
+            self.lr_handler.set_lr(state_dict['lr'])
+            self.model.load_partial_state_dict(state_dict['model_state_dict'], load_cls_embedding)
+
+    def save_model(self):
+        epoch = self.nEpochs
+        self.model.save_checkpoint(self.directory, self.title, epoch,
+                                      optimizer=self.optimizer,schedule=self.lr_handler.schedule)
 
 
     def create_optimizer(self):
@@ -50,8 +64,10 @@ class Trainer():
 
     def create_model(self):
         dim = (40, 64, 64)
-        if self.task.lower() == 'fine_tune':
-            self.model = Encoder_Transformer_finetune(dim,**self.kwargs)
+        if self.task.lower() == 'vae_reconstruction':
+            self.model = Encoder_Transformer_Decoder_VAE(dim,**self.kwargs)
+        elif self.task.lower() == 'video_vae':
+            self.model = VAE_with_VTN(dim,**self.kwargs)
         elif self.task.lower() == 'autoencoder_reconstruction':
             self.model = AutoEncoder(dim,**self.kwargs)
         elif self.task.lower() == 'transformer_reconstruction':
@@ -60,90 +76,100 @@ class Trainer():
             self.model = self.model.cuda()
 
     def training(self):
-        self.loss_history = {'intensity': [], 'perceptual':[], 'reconstruction':[]}
+        loss_history = {name:[] for name, current_loss_dict in self.losses.items() if current_loss_dict['is_active']}
         for epoch in range(self.nEpochs):
-            int_loss, perc_loss, rec_loss = self.train_epoch(epoch)
-            self.loss_history['intensity'].append(int_loss)
-            self.loss_history['perceptual'].append(perc_loss)
-            self.loss_history['reconstruction'].append(rec_loss)
+            epoch_losses = self.train_epoch(epoch)
+            for name in loss_history.keys():
+              loss_history[name] += epoch_losses[name]
+              print(name + f' loss {epoch_losses[name][-1]}')
 
             print('______epoch summary {}/{}_____\n'.format(epoch+1,self.nEpochs))
-            print(f'intensity loss {int_loss}, perceptual loss {perc_loss}, reconstruction loss {rec_loss}')
-        return self.loss_history
+            #print(f'intensity loss {int_loss[-1]}, perceptual loss {perc_loss[-1]}, reconstruction loss {rec_loss[-1]}')
+        return loss_history
 
 
     def train_epoch(self,epoch):
         self.train()
-        int_loss = 0
-        perc_loss = 0
-        rec_loss = 0
+        epoch_losses = {name:[] for name, current_loss_dict in self.losses.items() if current_loss_dict['is_active']}
 
         for batch_idx, input_dict in enumerate(tqdm(self.train_loader)):
            # self.writer.total_train_steps += 1
             self.optimizer.zero_grad()
             loss_dict, loss = self.forward_pass(input_dict)
-            
-            int_loss += loss_dict['intensity']
-            perc_loss += loss_dict['perceptual']
-            rec_loss += loss_dict['reconstruction']
+
+            for name in epoch_losses.keys():
+                epoch_losses[name].append(loss_dict[name])
 
             #torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
             loss.backward()
             self.optimizer.step()
             self.lr_handler.schedule_check_and_update()
+            #print(loss_dict)
 
-            #self.writer.write_losses(loss_dict, set='train')
-        return int_loss / len(self.train_loader), perc_loss / len(self.train_loader), rec_loss / len(self.train_loader)
+        return epoch_losses
 
     def train(self):
         self.mode = 'train'
         self.model = self.model.train()
 
-   # def get_last_loss(self):
-   #     if self.model.task == 'regression':
-   #         return self.writer.val_MAE[-1]
-   #     else:
-    #        return self.writer.total_val_loss_history[-1]
-
-    # def get_last_accuracy(self):
-    #     if hasattr(self.writer,'val_AUROC'):
-    #         return self.writer.val_AUROC[-1]
-    #     else:
-    #         return None
-
-    # def save_checkpoint_(self,epoch):
-    #     loss = self.get_last_loss()
-    #     accuracy = self.get_last_accuracy()
-    #     self.model.save_checkpoint(
-    #         self.writer.experiment_folder, self.writer.experiment_title, epoch, loss ,accuracy, self.optimizer ,schedule=self.lr_handler.schedule)
-
-
     def forward_pass(self,input_dict):
         #input_dict = {k:(v.cuda() if self.cuda else v) for k,v in input_dict.items()}
         if self.cuda:
-          input_dict = input_dict.cuda()
+            if isinstance(input_dict, dict):
+                input_dict = {k:(v.cuda() if self.cuda else v) for k,v in input_dict.items()}
+            else:
+                input_dict = input_dict.cuda()
+
         output_dict = self.model(input_dict)
         loss_dict, loss = self.aggregate_losses(input_dict, output_dict)
-
+        #if self.task == 'fine_tune':
+        #    self.compute_accuracy(input_dict, output_dict)
         return loss_dict, loss
 
 
     def aggregate_losses(self,input_dict,output_dict):
         final_loss_dict = {}
         final_loss_value = 0
+        if isinstance(input_dict, dict):
+          input = input_dict['fmri_seq']
+        else:
+          input = input_dict
         for loss_name, current_loss_dict in self.losses.items():
-            loss_func = getattr(self, 'compute_' + loss_name)
-            current_loss_value = loss_func(input_dict,output_dict)
-            if current_loss_value.isnan().sum() > 0:
-                warnings.warn('found nans in computation')
-                print('at {} loss'.format(loss_name))
-            lamda = current_loss_dict['factor']
-            factored_loss = current_loss_value * lamda
-            final_loss_dict[loss_name] = factored_loss.item()
-            final_loss_value += factored_loss
+            if current_loss_dict['is_active']:
+                loss_func = getattr(self, 'compute_' + loss_name)
+                current_loss_value = loss_func(input, output_dict)
+                if current_loss_value.isnan().sum() > 0:
+                    warnings.warn('found nans in computation')
+                    print('at {} loss'.format(loss_name))
+                lamda = current_loss_dict['factor']
+                factored_loss = current_loss_value * lamda
+                final_loss_dict[loss_name] = factored_loss.item()
+                final_loss_value += factored_loss
 
         final_loss_dict['total'] = final_loss_value.item()
         return final_loss_dict, final_loss_value
+
+    def compute_KL(self,input_dict,output_dict):
+        """
+            This function returns the value of KL(p1 || p2),
+            where p1 = Normal(mean_1, exp(log_std_1)**2), p2 is standard normal distribution.
+            Note that we consider the case of diagonal covariance matrix.
+        """
+        mean_1 = output_dict['mean_1']
+        log_std_1 = output_dict['log_std_1']
+
+        if 'mean_2' in output_dict.keys():
+          mean_2 = output_dict['mean_2']
+          log_std_2 = output_dict['log_std_2']
+        else:
+          mean_2 = torch.zeros_like(mean_1)
+          log_std_2 = torch.zeros_like(log_std_1)
+ 
+        var_1 = torch.exp(log_std_1) ** 2
+        var_2 = torch.exp(log_std_2) ** 2
+
+        KL = 2*(log_std_2 - log_std_1) - mean_1.shape[-1] + (mean_1 - mean_2)**2 / var_2 + var_1 / var_2
+        return torch.mean(KL / 2)
 
     def compute_reconstruction(self,input_dict,output_dict):
         fmri_sequence = input_dict[:,0].unsqueeze(1)
@@ -167,3 +193,4 @@ class Trainer():
         for name,value in kwargs.items():
             setattr(self,name,value)
         self.kwargs = kwargs
+
