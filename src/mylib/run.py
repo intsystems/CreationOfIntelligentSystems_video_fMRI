c66c370aaffed2bf327394c451e5b933a58d0350
Vosatorp <dmitry.protasov@gmail.com>
2022-12-13 03:18:33 +0300

    updated run.py (argparser, fix bugs)

--- src/mylib/run.py
+++ src/mylib/run.py
@@ -2,18 +2,40 @@ import torch
 from data_preprocess_and_load.dataset import fMRIDataset
 from torch.utils.data import DataLoader
 
-def main():
-    args = {'lr_init':1e-3, 'lr_gamma':0.97, 'lr_step':1000,
-        'weight_decay':1e-7, 'task':'transformer_reconstruction', 'cuda':False, 
-        'transformer_hidden_layers':2, 'batch_size':8,
-        'reconstruction_factor':1, 'perceptual_factor':1, 'intensity_factor':1,
-        'nEpochs':5, 'memory_constraint':0.1, 'loaded_model_weights_path':'AutoEncoder_last_epoch.pth'}
+from trainer import Trainer
+import argparse
 
-    dataset = fMRIDataset(seq_len=5)
-    train_dataloader = DataLoader(dataset=train_dataset, batch_size=8, shuffle=True, drop_last=True)
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lr_init", default=1e-3, type=float, help="initial lerning rate")
+    parser.add_argument("--lr_gamma", default=0.97, type=float)
+    parser.add_argument("--lr_step", default=1000, type=int)
+    parser.add_argument("--weight_decay", default=1e-7, type=float)
+    parser.add_argument("--task", default='transformer_reconstruction', type=str)
+    parser.add_argument("--cuda", default=False, type=bool)
+    parser.add_argument("--transformer_hidden_layers", default=2, type=int)
+    parser.add_argument("--batch_size", default=8, type=int)
+    parser.add_argument("--reconstruction_factor", default=1, type=float)
+    parser.add_argument("--perceptual_factor", default=1, type=float)
+    parser.add_argument("--intensity_factor", default=1, type=float)
+    parser.add_argument("--nEpochs", default=5, type=int)
+    parser.add_argument("--batch_size", default=8, type=int)
+    parser.add_argument("--memory_constraint", default=0.1, type=float)
+    parser.add_argument("--loaded_model_weights_path", default="AutoEncoder_last_epoch.pth", type=str)
+    args = parser.parse_args()
+    return args
 
+def main(args):
+    train_dataset = fMRIDataset(seq_len=5)
+    train_dataloader = DataLoader(
+        dataset=train_dataset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        drop_last=True
+    )
     trainer = Trainer(dataloader, **args)
     trainer.training()
 
 if __name__ == '__main__':
-    main()
+    args = get_args()
+    main(args)
